f7d20d88301bc1393601c75b48985323e029f442
zhuzhiou <zhuzhiou@qq.com>
2018-01-10 20:54:46 +0800

    提交代码

--- swagger2-sample/src/main/java/org/swagger/sample/endpoint/SampleController.java
+++ swagger2-sample/src/main/java/org/swagger/sample/endpoint/SampleController.java
@@ -5,8 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.hateoas.Resource;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +16,13 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.swagger.sample.vo.ErrorVo;
 import org.swagger.sample.vo.SampleVo;
+import springfox.documentation.annotations.ApiIgnore;
 
+import java.net.URI;
 import java.util.Collections;
 
-@Api(value = "用户相关接口", description = "在XML中配置Swagger2", protocols = "http")
-@RequestMapping("/users")
+@Api(value = "示例接口", description = "在XML中配置Swagger2", protocols = "http")
+@RequestMapping("/sample")
 @RestController
 public class SampleController {
 
@@ -31,23 +33,25 @@ public class SampleController {
             @ApiImplicitParam(name = "size", value = "每页记录数", required = true, paramType = "query")
     })
     @GetMapping(path = "", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Page<SampleVo> getUsers(@PageableDefault(sort = "username") Pageable pageable, SampleVo sampleVo) {
+    public Page<SampleVo> getUsers(@ApiIgnore @PageableDefault(sort = "username") Pageable pageable, @ApiIgnore SampleVo sampleVo) {
         return new PageImpl<>(Collections.emptyList(), pageable, 0);
     }
 
     @ApiOperation(value = "创建接口", notes = "POST示例", code = 201, response = SampleVo.class, responseHeaders = { @ResponseHeader(name = "Location", description = "URL")})
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "username", value = "用户名字", required = true, paramType = "body")
+            @ApiImplicitParam(name = "username", value = "用户名字", required = true, paramType = "body"),
+            @ApiImplicitParam(name = "password", value = "密码", required = true, paramType = "body"),
+            @ApiImplicitParam(name = "sex", value = "性别，男：male，女：female", required = true, paramType = "body")
     })
     @ApiResponses({
-            @ApiResponse(code = 201, message = "创建成功", response = SampleVo.class),
+            @ApiResponse(code = 201, message = "创建成功", responseHeaders = @ResponseHeader(name = "Location", response = URI.class)),
             @ApiResponse(code = 401, message = "无权访问", response = ErrorVo.class),
             @ApiResponse(code = 403, message = "认证失败", response = ErrorVo.class)
     })
     @PostMapping(path = "", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> addUser(SampleVo sampleVo) {
+    public Resource<SampleVo> addUser(@ApiIgnore  SampleVo sampleVo) {
         UriComponentsBuilder builder = ServletUriComponentsBuilder.fromCurrentRequestUri();
         UriComponents uriComponents = builder.build();
-        return ResponseEntity.created(uriComponents.toUri()).body(sampleVo);
+        return null;//ResponseEntity.created(uriComponents.toUri()).body(sampleVo);
     }
 }
--- swagger2-sample/src/main/java/org/swagger/sample/vo/SampleVo.java
+++ swagger2-sample/src/main/java/org/swagger/sample/vo/SampleVo.java
@@ -5,19 +5,19 @@ import io.swagger.annotations.ApiModelProperty;
 
 import java.io.Serializable;
 
-@ApiModel
+@ApiModel(description = "示例")
 @lombok.Data
 public class SampleVo implements Serializable {
 
-    @ApiModelProperty(name = "username", value = "账号，建议使用手机号、邮箱。")
-    private String username;
+    @ApiModelProperty(name = "field1", value = "字段一")
+    private String field1;
 
-    @ApiModelProperty(name = "password", value = "密码，使用sha256不可逆加密。")
-    private String password;
+    @ApiModelProperty(name = "field2", value = "字段二")
+    private String field2;
 
-    @ApiModelProperty(name = "sex", value = "性别，男：male，女：female。")
-    private String sex;
+    @ApiModelProperty(name = "field3", value = "字段三")
+    private String field3;
 
-    @ApiModelProperty(name = "createDate", value = "创建日期，使用2018-01-09T13:39:55格式的字符串。")
-    private String createDate;
+    @ApiModelProperty(name = "field4", value = "字段四")
+    private String field4;
 }
